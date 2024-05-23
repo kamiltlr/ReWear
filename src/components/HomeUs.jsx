d1562324be3dc61eed1858777e592251f2d2ee46
kamiltlr <thecichy12@gmail.com>
2024-05-23 20:47:11 +0200

    Add About Us  component

---
+++ src/components/HomeUs.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function HomeUs() {
+    return(
+        <div id='us' className='us'>
+            <div className="us-text">
+                <h1>O nas</h1>
+                <img src="../src/assets/Decoration.svg" alt="decoration"></img>
+                <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <img src="../src/assets/Signature.svg" alt="signature" className='us-signature' />
+            </div>
+
+            <div className="us-img">
+                <img src="../src/assets/People.jpg" alt="people" />
+            </div>
+        </div>
+    )
+}
+
+export default HomeUs;
