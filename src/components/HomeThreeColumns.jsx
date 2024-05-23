6e30d41a54b5ae26cc36d55fde16d34fac3a12af
kamiltlr <thecichy12@gmail.com>
2024-05-23 14:24:31 +0200

    Add Three Columns component

---
+++ src/components/HomeThreeColumns.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function HomeThreeColumns() {
+    return (
+      <div className='columns'>
+        <div className='columns-text'>
+            <h1>10</h1>
+            <p>Oddanych worków</p>
+            <span>Lorem ipsum dolor sit amet, consectetur adipi Pellentesque vel enim a elit vivera elementuma Aliquam erat volutpat.</span>
+        </div>
+
+        <div className='columns-text'>
+            <h1>5</h1>
+            <p>Wspartych organizacji</p>
+            <span>Lorem ipsum dolor sit amet, consectetur adipi Pellentesque vel enim a elit vivera elementuma Aliquam erat volutpat.</span>
+        </div>
+
+        <div className='columns-text'>
+            <h1>7</h1>
+            <p>Oddanych worków</p>
+            <span>Lorem ipsum dolor sit amet, consectetur adipi Pellentesque vel enim a elit vivera elementuma Aliquam erat volutpat.</span>
+        </div>
+      </div>
+    );
+  }
+
+export default HomeThreeColumns;
