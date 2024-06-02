dd13130df5a63afc1644540cf31595bc3d06cacd
kamiltlr <thecichy12@gmail.com>
2024-06-02 15:35:44 +0200

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-import HomeHeader from './HomeHeader';
-import HomeNavigation from './HomeNavigation';
+import Layout from './Layout';
 import HomeMain from './HomeMain';
 import HomeThreeColumns from './HomeThreeColumns';
 import HomeSimpleSteps from './HomeSimpleSteps';
 import HomeUs from './HomeUs';
 import HomeWhoWeHelp from './HomeWhoWeHelp';
 import HomeContact from './HomeContact';
+import Login from './Login';
+import Register from './Register';
 
 function Home() {
   return (
-    <div className='hero-container hero-img'>
-      <HomeHeader />
-      <HomeNavigation />
-      <HomeMain />
-      <HomeThreeColumns />
-      <HomeSimpleSteps />
-      <HomeUs />
-      <HomeWhoWeHelp />
-      <HomeContact />
+    <div>
+      <Routes>
+        <Route path="/" element={
+          <Layout>
+            <div className='hero-container hero-img'>
+              <HomeMain />
+              <HomeThreeColumns />
+              <HomeSimpleSteps />
+              <HomeUs />
+              <HomeWhoWeHelp />
+              <HomeContact />
+            </div>
+          </Layout>
+        } />
+        <Route path="/login" element={
+          <Layout>
+            <Login />
+          </Layout>
+        } />
+        <Route path="/register" element={
+          <Layout>
+            <Register />
+          </Layout>
+        } />
+      </Routes>
     </div>
   );
 }
