c0b11c202207d09a0fa9dc8b08e1dfbf7b8baa38
kamiltlr <thecichy12@gmail.com>
2024-06-02 17:24:55 +0200

    Update register.jsx

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,23 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-function Register() {
+const validateForm = (formData) => {
+  const errors = {};
+  
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!formData.email.trim()) {
+    errors.email = 'Pole nie może być puste';
+  } else if (!emailRegex.test(formData.email)) {
+    errors.email = 'Podany email jest nieprawidłowy!';
+  }
+  
+  if (!formData.password.trim()) {
+    errors.password = 'Pole nie może być puste';
+  } else if (formData.password.length < 6) {
+    errors.password = 'Podane hasło jest za krótkie!';
+  }
+
+  if (!formData.password2.trim()) {
+    errors.password2 = 'Pole nie może być puste';
+  } else if (formData.password2.length < 6) {
+    errors.password2 = 'Podane hasło jest za krótkie!';
+  } else if (formData.password !== formData.password2) {
+    errors.password2 = 'Hasła muszą być takie same!';
+  }
+  
+  return errors;
+};
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    password2: '' 
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+
+    const updatedErrors = { ...errors };
+    const fieldErrors = validateForm({ ...formData, [name]: value });
+    
+    if (fieldErrors[name]) {
+      updatedErrors[name] = fieldErrors[name];
+    } else {
+      delete updatedErrors[name];
+    }
+
+    setErrors(updatedErrors);
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const validationErrors = validateForm(formData);
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+  };
+
   return (
     <div className="register-container">
-      <h2>Rejestracja</h2>
-      <form>
-        <div>
-          <label>Email:</label>
-          <input type="email" name="email" required />
+      <div className="register-text">
+        <h2>Załóż konto</h2>
+        <img src="../src/assets/Decoration.svg" alt="decoration" />
+      </div>
+
+      <form onSubmit={handleFormSubmit}>
+        <div className="register-form">
+          <div className="register-email">
+            <label htmlFor="email">Email</label>
+            <input 
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+              className={errors.email ? 'input-error-email' : ''}
+            />
+            {errors.email && <p className="register-error">{errors.email}</p>}
+          </div>
+
+          <div className="register-password">
+            <label htmlFor="password">Hasło</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+              className={errors.password ? 'input-error-password' : ''}
+            />
+            {errors.password && <p className="register-error">{errors.password}</p>}
+          </div>
+
+          <div className="register-password2">
+            <label htmlFor="password2">Powtórz hasło</label>
+            <input
+              type="password"
+              id="password2"
+              name="password2"
+              value={formData.password2}
+              onChange={handleInputChange}
+              required
+              className={errors.password2 ? 'input-error-password' : ''}
+            />
+            {errors.password2 && <p className="register-error">{errors.password2}</p>}
+          </div>
         </div>
-        <div>
-          <label>Hasło:</label>
-          <input type="password" name="password" required />
+
+        <div className="register-buttons">
+          <Link to="/login" className="login-btn2">Zaloguj się</Link>
+          <button type="submit" className="register-btn2">Załóż konto</button>
         </div>
-        <div>
-          <label>Potwierdź Hasło:</label>
-          <input type="password" name="confirmPassword" required />
-        </div>
-        <button type="submit">Zarejestruj</button>
       </form>
     </div>
   );
