9460d80bc03aee376a4049501f752d752e3a6651
kamiltlr <thecichy12@gmail.com>
2024-06-03 00:09:23 +0200

    Update Logout.jsx

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { signOut } from 'firebase/auth';
+import { useNavigate, Link } from 'react-router-dom';
+import { auth } from './Firebase';
 
-function Logout() {
-    return (
-      <div className='logut'>
+const Logout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const logout = async () => {
+      await signOut(auth);
+      navigate('/wylogowano');
+    };
+    logout();
+  }, [navigate]);
+
+  return (
+    <div className='logut'>
         <div className='logout-text'>
             <h2>Wylogowanie nastąpiło pomyślnie!</h2>
             <img src="../src/assets/Decoration.svg" alt="decoration" />
             <Link to="/" className="home-btn">Strona główna</Link>
         </div>
       </div>
-    );
-  }
+  );
+}
 
 export default Logout;
