f4d100639da859fd182c9c9fc2c1e2d29fef61e4
kamiltlr <thecichy12@gmail.com>
2024-06-03 00:10:06 +0200

    Update HomeHeader.jsx

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { onAuthStateChanged, auth } from './Firebase';
 
-function HomeHeader() {
+const HomeHeader = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <div className="hero-buttons">
-    <Link to="/logowanie" className="login-button">Zaloguj</Link>
-    <Link to="/rejestracja" className="login-button">Załóż konto</Link>
-  </div>
+    <header>
+      <nav className="hero-buttons">
+        {user ? (
+          <>
+            <span className='hero-account'>Cześć! {user.email}!</span>
+            <Link to="/oddaj-rzeczy" className="hero-btn">Oddaj rzeczy</Link>
+            <Link to="/wylogowano" className="hero-logout">Wyloguj</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/logowanie" className="hero-login">Zaloguj</Link>
+            <Link to="/rejestracja" className="hero-register">Załóż konto</Link>
+          </>
+        )}
+      </nav>
+    </header>
   );
 }
 
