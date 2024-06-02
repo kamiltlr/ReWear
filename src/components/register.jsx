4487dc1e662a7944b1ea39e8b8e69864ee593342
kamiltlr <thecichy12@gmail.com>
2024-06-02 15:36:14 +0200

    Update register.jsx

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 function Register() {
   return (
-    <div>
-      <h1>Rejestracja</h1>
+    <div className="register-container">
+      <h2>Rejestracja</h2>
+      <form>
+        <div>
+          <label>Email:</label>
+          <input type="email" name="email" required />
+        </div>
+        <div>
+          <label>Hasło:</label>
+          <input type="password" name="password" required />
+        </div>
+        <div>
+          <label>Potwierdź Hasło:</label>
+          <input type="password" name="confirmPassword" required />
+        </div>
+        <button type="submit">Zarejestruj</button>
+      </form>
     </div>
   );
 }
