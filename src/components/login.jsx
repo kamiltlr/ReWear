655381c3c5724bcb91d3f18825a17596362b4776
kamiltlr <thecichy12@gmail.com>
2024-06-02 15:36:29 +0200

    Update login.jsx

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <div>
-      <h1>Logowanie</h1>
+    <div className="login-container">
+      <div className="login-text">
+        <h2>Zaloguj się</h2>
+        <img src="../src/assets/Decoration.svg" alt="decoration"></img>
+      </div>
+
+      <form>
+        <div className="login-form">
+          <div className='login-email'>
+            <label>Email</label>
+            <input type="email" name="email" required />
+          </div>
+
+          <div className='login-password'>
+            <label>Hasło</label>
+            <input type="password" name="password" required />
+          </div>
+        </div>
+
+        <div className="login-buttons">
+          <Link to="/register" className="register-btn">Załóż konto</Link>
+          <button type="submit" className='login-btn'>Zaloguj się</button>
+        </div>
+      </form>
     </div>
   );
 }
